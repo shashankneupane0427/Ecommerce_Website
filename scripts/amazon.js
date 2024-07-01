c81c9357ecb1606ad188eb8e4c0a67a73b5a690b
Shashank Neupane <suryaneupane0427@gmail.com>
2024-07-01 14:16:52 +0545

    update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -58,6 +58,8 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
+
+    let addedMessageTimeoutId;
     button.addEventListener('click', () => {
       const {productId} = button.dataset;
 
@@ -92,5 +94,21 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
+
+        const addedMessage = document.querySelector(
+          `.js-added-to-cart-${productId}`
+        );
+
+        addedMessage.classList.add('added-to-cart-visible');
+
+        if (addedMessageTimeoutId) {
+          clearTimeout(addedMessageTimeoutId);
+        }
+  
+        const timeoutId = setTimeout(() => {
+          addedMessage.classList.remove('added-to-cart-visible');
+        }, 2000);
+
+        addedMessageTimeoutId = timeoutId;
     });
   });
