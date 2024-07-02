65548f05f747595ed2ac87225bc4b603866d0a68
Shashank Neupane <suryaneupane0427@gmail.com>
2024-07-02 14:56:07 +0545

    update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -40,7 +40,7 @@ cartSummaryHTML +=  `
             <span class="update-quantity-link link-primary">
             Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
             </span>
         </div>
@@ -99,3 +99,11 @@ cartSummaryHTML +=  `
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
 
+
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+            const productId = link.dataset.productId;
+            console.log(productId);
+        });
+    });
