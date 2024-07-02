a41a7ec7e472c070afea516f0bc09e3b9b4e36fc
Shashank Neupane <suryaneupane0427@gmail.com>
2024-07-02 15:09:38 +0545

    update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
     });
 
 cartSummaryHTML +=  `
-<div class="cart-item-container">
+<div class="cart-item-container  js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -106,6 +106,9 @@ document.querySelectorAll('.js-delete-link')
             const productId = link.dataset.productId;
             console.log(productId);
             removeFromCart(productId);
-            console.log(cart);
+    
+           const container = document.querySelector(`.js-cart-item-container-${productId}`)
+
+           container.remove();
         });
     });
