9ec72c3059109733f08b560c257972cc74dddf65
Shashank Neupane <suryaneupane0427@gmail.com>
2024-07-02 15:38:12 +0545

    update all good

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -30,6 +38,9 @@ export function addToCart(productId) {
         quantity
       });
     }
+
+
+    saveToStorage();
   }
 
   export function removeFromCart(productId) {
@@ -42,4 +53,6 @@ export function addToCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
   }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -110,5 +110,19 @@ document.querySelectorAll('.js-delete-link')
            const container = document.querySelector(`.js-cart-item-container-${productId}`)
 
            container.remove();
+
+           updateCartQuantity();
         });
     });
+
+    function updateCartQuantity() {
+        let cartQuantity = 0;
+        cart.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+          });
+        
+          document.querySelector('.js-return-to-home-link')
+            .innerHTML = `${cartQuantity} items`;
+        }
+
+        updateCartQuantity();
