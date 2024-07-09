637a8dd30f388d9eefd0a76685cf58180802cad9
Shashank Neupane <suryaneupane0427@gmail.com>
2024-07-09 16:25:11 +0545

    update

--- data/cart.js
+++ data/cart.js
@@ -4,9 +4,11 @@ if (!cart) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
+    deliveryOptionId: '1'
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
@@ -35,7 +37,8 @@ export function addToCart(productId) {
     } else {
       cart.push({
         productId,
-        quantity
+        quantity,
+        deliveryOptionId: '1'
       });
     }
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,14 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
+dayjs();
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+deliveryDate.format('dddd, MMMM D');
+console.log(deliveryDate.format('dddd, MMMM D'));
+
 
 let cartSummaryHTML = '';
 
